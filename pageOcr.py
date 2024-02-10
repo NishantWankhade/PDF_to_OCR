cf4270b59a152c03029f4371fa0e958b5ba62064
NGW334 <ngw334@gmail.com>
2024-02-10 12:16:26 +0530

    OCR using APIs

--- pageOcr.py
+++ pageOcr.py
@@ -7,18 +7,24 @@ payload = {'language': 'hindi',
 'modality': 'printed',
 'layout_model': 'v2_doctr'}
 
-files=[
- ('image',(
- '1.jpg',
- open('1.png','rb'),
- 'image/jpeg')
- )
-]
-resp = requests.post(url, headers={}, data=payload, files=files)
 
-# print(resp.text)
-req_json = json.loads(resp.text)
-print(req_json["text"])
+count = 0
 
-with open("file.txt", "w", encoding="utf-8") as f:
-    f.write(req_json["text"])
+while count < 516 :
+    files=[
+    ('image',(
+    '1.jpg',
+    open(f'Bharatiya-SahityaShastra/{count}.jpg','rb'),
+    'image/jpeg')
+    )
+    ]
+    resp = requests.post(url, headers={}, data=payload, files=files)
+
+    req_json = json.loads(resp.text)
+    print(req_json["text"])
+
+    with open(f"Ocred Book/{count}.txt", "w", encoding="utf-8") as f:
+        f.write(req_json["text"])
+    
+    print(f"{count} done !")
+    count = count + 1
