416388957e10f1e547bddf7a44efce245619c28c
NGW334 <ngw334@gmail.com>
2024-02-16 10:43:58 +0530

    Better organization

--- pdf_to_image.py
+++ pdf_to_image.py
@@ -1,6 +1,11 @@
 from pdf2image import convert_from_path
  
-images = convert_from_path('book.pdf', poppler_path="poppler-23.11.0/Library/bin")
- 
-for i in range(len(images)):
-    images[i].save('Bharatiya-SahityaShastra/'+ str(i) +'.jpg', 'JPEG')
+def convert_to_images(pdf_path, image_path, book_name):
+    images = convert_from_path(pdf_path, poppler_path="poppler-23.11.0/Library/bin")
+    
+    for i in range(len(images)):
+        images[i].save(f"{image_path}/{book_name}/images/" + str(i) +'.jpg', 'JPEG')
+
+
+if __name__ == "__main__":
+    print("Pdf_to_image")
