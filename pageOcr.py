855224ec2579b29c7d3db4f1041801ce99c80e58
NGW334 <ngw334@gmail.com>
2024-02-19 11:30:07 +0530

    Final working version on the basis of PDF path only

--- pageOcr.py
+++ pageOcr.py
@@ -4,28 +4,27 @@ import json
 import os
 from file_paths import retrive_file_paths
 from pdf_to_image import convert_to_images
+import time
 
 
-def pdf_to_text(pdf_path, output_text_file, image_dir, book_name):
+def pdf_to_text(output_path):
 
-    os.makedirs(f"{image_dir}/{book_name}/text", exist_ok=True)
-
-    convert_to_images(pdf_path, image_dir, book_name) # call Convert PDF to images function
-
+    os.makedirs(f"{output_path}/text", exist_ok=True)
     print("Performing OCR on images...")
     url = "http://bhasha.iiit.ac.in/pageocr/api"
     payload = {'language': 'hindi',
                'version': 'v4_robust',
                'modality': 'printed',
                'layout_model': 'v2_doctr'}
-
+    
+    images = os.listdir(f"{output_path}/images")
     file_paths = []
-    for i in range(len(images)):
-        files = [('image', (f'{i}.jpg', open(f'{image_dir}/{book_name}/images/{i}.jpg', 'rb'), 'image/jpeg'))]
+    for i in range(1, len(images)):
+        files = [('image', ('1.jpg', open(f'{output_path}/images/{i + 1}.jpg', 'rb'), 'image/jpeg'))]
         resp = requests.post(url, headers={}, data=payload, files=files)
         req_json = json.loads(resp.text)
 
-        text_file_path = f"{image_dir}/{book_name}/text/{i}.txt"
+        text_file_path = f"{output_path}/text/{i+1}.txt"
         file_paths.append(text_file_path)
 
         with open(text_file_path, 'w', encoding='utf-8') as f:
@@ -36,24 +35,33 @@ def pdf_to_text(pdf_path, output_text_file, image_dir, book_name):
 
     print("Combining text files into a single text file...")
     file_paths.sort(key=lambda x: int(x.split('/')[-1].split('.')[0]))
-    with open(output_text_file, 'w', encoding='utf-8') as outfile:
+    with open(output_path+"/ocr_by_Bhashini.txt", 'w', encoding='utf-8') as outfile:
         for page_number, file_path in enumerate(file_paths, start=1):
             with open(file_path, 'r', encoding='utf-8') as infile:
                 outfile.write(infile.read() + '\n')  # add a newline between files if needed
         print("Combined text file created.")
 
-    # Optionally to delete the text files and imagess
-    # Commented out for now
-    # for file_path in file_paths:
-    #     os.remove(file_path)
-    #
-    # for i in range(len(images)):
-    #     os.remove(f'{image_dir}/{book_name}/images/{i}.jpg')
+    # # Optionally to delete the text files and imagess
+    # # Commented out for now
+    for file_path in file_paths:
+        os.remove(file_path)
+    
+    for i in range(1,len(images)):
+        os.remove(f'{output_path}/images/{i}.jpg')
 
 
-pdf_path = "" # Path to the PDF file
-output_text_file = "" # Path to the output text file
-image_dir = "" # Path to the directory where images and text files will be saved
-book_name = "" # Name of the book, used to create subdirectories in "image_dir
+if __name__ == "__main__" :
+    
+    # Path to the pdf
+    pdf_path = "" 
+    output_path = pdf_path.replace(".pdf", "")
 
-pdf_to_text(pdf_path, output_text_file, image_dir, book_name)
+    start = time.perf_counter()
+    
+    convert_to_images(pdf_path, output_path)
+    
+    pdf_to_text(output_path)
+
+    end = time.perf_counter()
+
+    print(round(end - start, 2))
--- pdf_to_image.py
+++ pdf_to_image.py
@@ -1,16 +1,13 @@
 from pdf2image import convert_from_path
+import os
 
+def convert_to_images(pdf_path,output_path):
 
-def convert_to_images(pdf_path, image_dir, book_name):
-
-    os.makedirs(f"{image_dir}/{book_name}/images", exist_ok=True)
+    os.makedirs(f"{output_path}/images", exist_ok=True)
 
     print("Converting PDF to images...")
     images = convert_from_path(pdf_path, poppler_path="poppler-23.11.0/Library/bin")
 
     print("Saving images...")
     for i, image in enumerate(images):
-        image.save(f"{image_dir}/{book_name}/images/{i}.jpg", 'JPEG')
-        print(f"Image {i + 1} saved.")
-
-    print("Performing OCR on images...")
+        image.save(f"{output_path}/images/{i + 1}.jpg", 'JPEG')
