0cb9c2abb33afa7f7bb7fbcdf308d8ce263379f5
KoushikTeja <koushik.viluvala@gmail.com>
2024-02-29 14:59:12 +0530

    included edit distance and 4 files were corrected

--- file_paths.py
+++ file_paths.py
@@ -12,3 +12,24 @@ def retrive_file_paths(number_of_files, root_dir):
                 count += 1
                 file_paths_list.append(os.path.join(file_path, txt))
     return file_paths_list
+
+
+
+
+def retrieve_file_paths_text(number_of_files, root_dir):
+    file_paths_list = []
+    count = 0
+
+    # List all files in the root directory
+    files = os.listdir(root_dir)
+
+    for file in files:
+        file_path = os.path.join(root_dir, file)
+
+        # Check if the file is a text file and the limit is not reached
+        if file.endswith('.txt') and count < number_of_files:
+            count += 1
+            file_paths_list.append(file_path)
+
+    return file_paths_list
+
