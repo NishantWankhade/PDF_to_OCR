ee76110a9a205f1c790e7b3785606147efa4af97
NGW334 <ngw334@gmail.com>
2024-03-07 23:35:37 +0530

    Optimizing the cluster

--- README.md
+++ README.md
@@ -2,3 +2,14 @@
 Analysis of the Meta Deta
 
 Download the meta_hindi.csv in this repo from [Drive link](https://drive.google.com/file/d/1MGRPYxIG_MabwcW6CaMPqPWBAQ-Xjqci/view)
+
+---> Threshold count --> Output ==> 10k 
+--> Creator > ppi>=300 
+--> Randomnly 1 book from each of the 10k clusters 
+
+
+Total Unique Subject  --> 13902
+Total Unique Creators ---> ~33500 
+
+
+--> Removing multiple languages 
--- analyze_csv.py
+++ analyze_csv.py
@@ -16,19 +16,38 @@ def create_csv_cluster(csv_path, row_num):
         write_cluster_files(cluster_dict, name)
 
 #Cluster on the basis of Creator
-def write_cluster_files(cluster_dict, name):
+def write_cluster_files(cluster_dict, name, optimized = ""):
     os.makedirs("analysis",exist_ok=True)
-    with open(f"analysis/{name}.csv", 'w', newline='', encoding='utf-8') as f:
+    with open(f"analysis/{optimized}_{name}.csv", 'w', newline='', encoding='utf-8') as f:
         csv_writer = csv.writer(f)
         csv_writer.writerow([f"{name}", "1"])
         for i in cluster_dict :
             csv_writer.writerow([i, cluster_dict[i]])
 
-if __name__ == "__main__":
-    csv_path = "meta_hindi.csv"
-    csv.field_size_limit(100000000)
+#Removing Punctuations and Converting to lowercase
+def optimize_csv(csv_path):
+    with open(csv_path, 'r', newline='', encoding='utf-8') as csvfile:
+        csv_file = csv.reader(csvfile)
+        cluster_dict = {}
+        first = 0
+        for row in csv_file:
+            name = row[0].lower().replace(" ","")
+            for x in [',', '"' , '\'','.', ';', ':' ,'-']:
+                name = name.replace(x, "")
+            cluster_dict[name] = cluster_dict.get(name, 0) + int(row[1])
+        
+        print(len(cluster_dict))
+        write_cluster_files(cluster_dict, "subject" , "optimized")
 
-    print("Creating CSV cluster")
-    for i in range(2, 108):
-        create_csv_cluster(csv_path,i)
+
+if __name__ == "__main__":
+    csv_path = "analysis/_subject.csv"
+    csv.field_size_limit(100000000)
+    optimize_csv(csv_path)
+
+    # print("Creating CSV cluster")
+    # for i in range(2, 108):
+    # create_csv_cluster(csv_path,95)
+
+
 
